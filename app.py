69a2b76f1b48681c35bf7d642ce8fc578a5b58d9
YYSU <jim123820@gmail.com>
2021-07-26 23:55:06 +0800

    Add config and response

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.config.from_object('config')
 
 @app.route('/hello')
 def hello():
-    return 'Hello, World!'
+    headers = {
+        'content-type': 'text/plain'
+    }
+    return '<h1> Yo !</h1>', 200, headers
 
-# 另一種路由的實現方式
-# app.add_url_rule('/hello', view_func=hello)
 
-app.run(debug=True)
+# 通常在生產環境會使用 nginx + uwsgi，不會直接使用 flask sever
+# https://www.maxlist.xyz/2020/05/06/flask-wsgi-nginx/
+# 因此需要判斷
+if __name__ == "__main__":
+    app.run(host=app.config['HOST'], port=app.config['PORT'], debug=app.config['DEBUG'])
